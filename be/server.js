8b9139a43b30252791975a4b85d9d9195aa5f004
tomer <tomerd2010@ofekschool.org.il>
2026-05-07 12:35:22 +0300

    done

---
+++ be/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+
+// מאפשר קריאת JSON מהבקשה
+app.use(bodyParser.json());
+
+// מאפשר CORS כדי שה-FE יוכל לשלוח בקשות
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+// הפעלת השרת
+app.listen(port, () => {
+    console.log(`השרת רץ: http://localhost:${port}`);
+});
---
+++ fe/main.js
@@ -0,0 +1,44 @@
+function func() {
+    let p = document.querySelector(".main")
+    p.style.color="red"
+}
+
+let p = document.querySelector("#larry")
+
+function hide()
+{
+    p.style.display = "none";
+}
+
+function show()
+{
+    p.style.display = "block";
+}
+
+let h = document.querySelector("#fs")
+
+function buttonshitnoneblock()
+{
+    if (h.style.display === "none")
+    {
+        h.style.display = "block";
+    }
+    else
+    {
+        h.style.display = "none";
+    }
+}
+
+let h2 = document.querySelector("#overme")
+
+function mouseover1()
+{
+    h2.style.textDecoration = "underline"
+}
+
+function out()
+{
+    h2.style.textDecoration = "none"
+}
+
+function tabTexts
