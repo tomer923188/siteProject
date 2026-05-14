2259bdfbeb8037fe3ce98ed9014148546f3c01bc
tomer923188 <tomerd2010@ofekschool.org.il>
2026-05-14 12:50:45 +0000

    Add files via upload

--- be/server.js
+++ be/server.js
@@ -3,6 +3,9 @@ const app = express();
 const path = require("path");
 const port = 3000;
 
+let fname = 'admin';
+let lname = '1234';
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../fe")));
 
@@ -20,6 +23,20 @@ app.get("/get", (req, res) => {
 app.post("/contact", (req, res) => {
     console.log("POST request data:", req.body);
     res.send("Form data received");
+});
+
+app.post("/login", (req, res) => {
+    console.log("POST request data:", req.body);
+    res.send("Form data received");
+
+    const { username, password } = req.body;
+    if(username==="admin"&&password===12345){
+        console.log("data correct");
+    }
+    else
+    {
+        console.log("data incorrect");
+    }
 });
 
 
